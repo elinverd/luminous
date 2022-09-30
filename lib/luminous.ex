d097785134a8517a6dccd8030034a5cd1399795f
Kyriakos Kentzoglanakis <kyriakos@kentzoglanakis.org>
2022-09-30 18:43:44 +0300

    Remove boilerplate code

--- lib/luminous.ex
+++ lib/luminous.ex
@@ -2,17 +2,4 @@ defmodule Luminous do
   @moduledoc """
   Documentation for `Luminous`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Luminous.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
