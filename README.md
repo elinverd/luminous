9298acaf9108dfc34e7cb0fbcf0d45d146586bcd
Kyriakos Kentzoglanakis <kyriakos@kentzoglanakis.org>
2023-02-21 12:31:21 +0200

    v1.0.1

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ The package can be installed from `hex.pm` as follows:
 ```elixir
 def deps do
   [
-    {:luminous, "~> 1.0.0"}
+    {:luminous, "~> 1.0.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Luminous.MixProject do
   def project do
     [
       app: :luminous,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: ">= 1.12.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
